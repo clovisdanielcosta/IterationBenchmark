fffd69fe86db2c181d1c4234883863046f8fc833
Clóvis Daniel Costa <clovisdaniel@hotmail.com>
2023-12-05 11:10:37 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Runtime.InteropServices;
+
+var Rng = new Random(90500);
+
+var lista = Enumerable.Range(1, 500000000).Select(x => Rng.Next()).ToList();
+
+var agora = DateTime.Now;
+
+
+Console.WriteLine("Foreach\nInício: " + agora);
+
+foreach (var item in lista)
+{
+
+}
+
+Console.WriteLine($"Duração: {DateTime.Now - agora}\n\n");
+
+
+
+lista = Enumerable.Range(1, 100000000).Select(x => Rng.Next()).ToList();
+agora = DateTime.Now;
+
+Console.WriteLine("CollectionsMarshal\nInício: " + agora);
+
+var asSpan = CollectionsMarshal.AsSpan(lista);
+
+foreach (int item in CollectionsMarshal.AsSpan(lista))
+{
+    
+}
+
+Console.WriteLine($"Duração: {DateTime.Now - agora}\n\n");
+
+
+
